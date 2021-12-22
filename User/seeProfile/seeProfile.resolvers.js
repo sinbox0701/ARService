14f3bb7eedfab75c1053ca0e3c93f732901f0c69
sinbox0701 <psh090953@gmail.com>
2021-12-22 14:30:39 +0900

    seeProfile 변경

--- User/seeProfile/seeProfile.resolvers.js
+++ User/seeProfile/seeProfile.resolvers.js
@@ -3,16 +3,16 @@ import client from "../../client"
 
 export default {
     Query:{
-        seeProfile: protectedResolver(async (_,{id},{loggedInUser})=>{
+        seeProfile: protectedResolver(async (_,{nickname},{loggedInUser})=>{
             if(!loggedInUser.id){
                 return null;
             }
             const user = await client.user.findUnique({
                 where:{
-                    id
+                    nickname
                 }
             });
-            if(!user.id){
+            if(!user){
                 return null;
             }
             return user;
