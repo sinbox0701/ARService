90d8bb59f3de6e1753aaf4610389500e54fcd8e9
sinbox0701 <psh090953@gmail.com>
2022-01-31 17:30:51 +0900

    변경

--- server.js
+++ server.js
@@ -45,79 +45,15 @@ app.use("/static", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const users = [
-    { id: 1, name: "유저1" },
-    { id: 2, name: "유저2" },
-    { id: 3, name: "유저3" }
-];
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
 
 io.on('connection', (socket) => {
+    socket.on("join",(nickname,done)=>{
+        socket.join(nickname);
+        done();
+        socket.to(nickname).emit("welcome");
+    })
     console.log('a user connected')
 })
-
-
-app.get("/api/users", (req, res) => {
-    //유저 정보 반환
-    res.json({ ok: true, users: users });
-});
-app.get("/api/users/user", (req, res) => {
-    const user_id = req.query.user_id
-    const user = users.filter(data => data.id == user_id);
-
-    res.json({ ok: false, user: user })
-});
-app.get("/api/users/:user_id", (req, res) => {
-    const user_id = req.params.user_id
-    const user = users.filter(data => data.id == user_id);
-
-    res.json({ ok: true, user: user })
-});
-app.post("/api/users/add", (req, res) => {
-    const { id, name } = req.body;
-    const user = users.concat({ id, name });
-
-    res.json({ ok: true, users: user });
-})
-app.put("/api/users/update", (req, res) => {
-    const { id, name } = req.body;
-    const user = users.map(data => {
-        if (data.id == id) {
-            data.name = name;
-        }
-        return {
-            id: data.id,
-            name: data.name
-        };
-    });
-
-    res.json({ ok: true, users: user });
-});//전체 데이터 수정
-app.patch("/api/user/update/:user_id", (req, res) => {
-    const { user_id } = req.params;
-    const { name } = req.body;
-    const user = users.map(data => {
-        if (data.id == user_id) {
-            data.name = name;
-        }
-
-        return {
-            id: data.id,
-            name: data.name
-        };
-    });
-
-    res.json({ ok: true, users: user });
-});//단일 데이터 수정
-app.delete("/api/user/delete", (req, res) => {
-    const user_id = req.query.user_id;
-    const user = users.filter(data => data.id != user_id);
-
-    res.json({ ok: true, users: user });
-});
 
 // server.listen({ port: restPORT }, () => {
 //     console.log(`Server is running on http://localhost:${restPORT}/graphql`)
